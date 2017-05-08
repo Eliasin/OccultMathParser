b1e8363ff181f61272eff8715bf948c5e0207e45
Steven Pham <steven.pham.8.2000@gmail.com>
2017-05-08 13:48:09 -0400

    Condense all expression headers

---
+++ include/MathExpressions.hpp
@@ -0,0 +1,11 @@
+#ifndef MATH_EXPRESSIONS
+#define MATH_EXPRESSIONS
+
+#include "Expression.hpp"
+#include "SumExpression.hpp"
+#include "DifferenceExpression.hpp"
+#include "ProductExpression.hpp"
+#include "QuotientExpression.hpp"
+#include "NegationExpression.hpp"
+
+#endif
